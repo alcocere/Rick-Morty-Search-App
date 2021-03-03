d9d9a9cbca296cfb67f2d5cefb0cb0f8c0ca5058
alcocere <elisa.alcocerm@gmail.com>
2021-03-03 20:05:28 +0100

    Add prevent default event in form

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -3,9 +3,12 @@ import FilterByName from "./FilterByName";
 import FilterBySpecie from "./FilterBySpecie";
 
 const Filters = (props) => {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    };
     return (
         <section>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <FilterByName handleFilter={props.handleFilter} />
                 <FilterBySpecie handleFilter={props.handleFilter} />
             </form>
