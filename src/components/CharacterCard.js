473070a5b8ea62234fc5ac54f65359318a9e2709
alcocere <elisa.alcocerm@gmail.com>
2021-03-03 23:11:49 +0100

    Add unique route id for each character

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "react-router-dom";
 import "../stylesheets/layout/Cards.scss";
 
 const CharacterCard = (props) => {
@@ -6,14 +7,16 @@ const CharacterCard = (props) => {
     return (
         <>
             <article className="card">
-                <img
-                    src={props.character.image}
-                    alt={"Image from " + props.character.name}
-                    title={"Image from " + props.character.name}
-                    className="card__image"
-                />
-                <h2 className="card__name">{props.character.name}</h2>
-                <p className="card__species">{props.character.species}</p>
+                <Link to={`/character/${props.character.id}`}>
+                    <img
+                        src={props.character.image}
+                        alt={"Image from " + props.character.name}
+                        title={"Image from " + props.character.name}
+                        className="card__image"
+                    />
+                    <h2 className="card__name">{props.character.name}</h2>
+                    <p className="card__species">{props.character.species}</p>
+                </Link>
             </article>
         </>
     )
