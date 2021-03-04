97f42fc404c0dbb76a95df7a9c33fbc565a9a551
alcocere <elisa.alcocerm@gmail.com>
2021-03-04 11:29:12 +0100

    Add not-found component

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,5 +1,6 @@
 import React from "react"
 import CharacterCard from "./CharacterCard";
+import CharacterNotFound from "./CharacterNotFound";
 
 const CharacterList = props => {
     const character = props.characters.map(character => {
@@ -8,12 +9,17 @@ const CharacterList = props => {
                 <CharacterCard character={character} />
             </li>
         );
-    }
-    );
+    });
+    const notFound = () => {
+        if (character.length === 0) {
+            return <CharacterNotFound nameFilter={props.nameFilter} />
+        }
+    };
 
     return (
         <section className="cards-section">
             <ul className="cards-container">{character}</ul>
+            {notFound()}
         </section>
     );
 };
--- src/components/CharacterNotFound.js
+++ src/components/CharacterNotFound.js
@@ -1,8 +1,19 @@
-// import React from "react";
-// import PropTypes from "prop-types";
+import React from "react";
+import PropTypes from "prop-types";
 
 
+const CharacterNotFound = (props) => {
+    return (
+        <div className="notFound-container">
+            <p className="notFound-container__text">Sorry, there isn't any character in this Universe with the name {props.nameFilter}</p>
+            <p className="notFound-container__subtext">Wanna try again??</p>
+            <img
+                src="https://media.giphy.com/media/bGtMLIa5RQzVm/giphy.gif"
+                alt="Not Found gif"
+                className="notFound-container__img" />
+        </div>
+    );
+};
 
 
-
-// export default CharacterNotFound;
+export default CharacterNotFound;
